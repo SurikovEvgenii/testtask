1de4973268f0c51e79fae5f5f2a134226e3fbcf1
Evgenii Surikov <surikov96@mail.ru>
2025-01-19 15:09:31 +0700

    Remove repeated code. Add method write()

--- src/main/java/ru/surikov/handlers/FileHandler.java
+++ src/main/java/ru/surikov/handlers/FileHandler.java
@@ -4,15 +4,15 @@ import ru.surikov.controllers.CheckTypeOfDataController;
 import ru.surikov.entities.DataLists;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 
 public class FileHandler {
 
     public static void readFile(List<String> fileNameList) {
-
         Iterator<String> iterator = fileNameList.iterator();
-
         while (iterator.hasNext()) {
             try (BufferedReader bufferedReader = new BufferedReader(new FileReader(iterator.next()))) {
                 while (bufferedReader.ready()) {
@@ -37,37 +37,35 @@ public class FileHandler {
         }
     }
 
-    public static void writeFile(String path, String prefix, boolean append) {
-
+    public static void writeFile(String savePath, String prefix, boolean append) {
         try {
+            Files.createDirectories(Paths.get(savePath));
+            File fileDouble = Paths.get(savePath, prefix + "double").toFile();
+            File fileLong = Paths.get(savePath, prefix + "integer").toFile();
+            File fileString = Paths.get(savePath, prefix + "string").toFile();
+
             if (!DataLists.getDoubleList().isEmpty()) {
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(prefix + "double.txt", append));
-                Iterator iterator = DataLists.getDoubleList().iterator();
-                while (iterator.hasNext()) {
-                    bufferedWriter.write(iterator.next().toString());
-                    bufferedWriter.newLine();
-                }
-                bufferedWriter.flush();
+                write(fileDouble, append, DataLists.getDoubleList().iterator());
             }
             if (!DataLists.getLongList().isEmpty()) {
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(prefix + "integer.txt", append));
-                Iterator iterator = DataLists.getLongList().iterator();
-                while (iterator.hasNext()) {
-                    bufferedWriter.write(iterator.next().toString());
-                    bufferedWriter.newLine();
-                }
-                bufferedWriter.flush();
+                write(fileLong, append, DataLists.getLongList().iterator());
             }
             if (!DataLists.getStringList().isEmpty()) {
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(prefix + "string.txt", append));
-                Iterator iterator = DataLists.getStringList().iterator();
-                while (iterator.hasNext()) {
-                    bufferedWriter.write((String) iterator.next());
-                    bufferedWriter.newLine();
-                }
-                bufferedWriter.flush();
+                write(fileString, append, DataLists.getStringList().iterator());
             }
         } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void write(File file, boolean append, Iterator iterator)  {
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, append))){
+            while (iterator.hasNext()) {
+                bufferedWriter.write(iterator.next().toString());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.flush();
+        } catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
