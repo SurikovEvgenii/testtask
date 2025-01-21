7cf1feb9524dfe19bd66812ad69cc2dee8dc951b
Evgenii Surikov <surikov96@mail.ru>
2025-01-21 14:59:07 +0700

    Replace variable to reference invoke

--- src/test/java/ru/surikov/controllers/CheckTypeOfDataControllerTest.java
+++ src/test/java/ru/surikov/controllers/CheckTypeOfDataControllerTest.java
@@ -2,44 +2,42 @@ package ru.surikov.controllers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CheckTypeOfDataControllerTest {
 
-    CheckTypeOfDataController checkTypeOfDataController = new CheckTypeOfDataController();
-
     @Test
     void isDoubleTest(){
-        assertEquals(true, checkTypeOfDataController.isDouble("2.043"));
-        assertEquals(true, checkTypeOfDataController.isDouble("2.044E-10"));
-        assertEquals(true, checkTypeOfDataController.isDouble("-2.045"));
-        assertEquals(true, checkTypeOfDataController.isDouble("-2.046E-10"));
-        assertEquals(true, checkTypeOfDataController.isDouble("0.0"));
-        assertEquals(false, checkTypeOfDataController.isDouble("2043"));
-        assertEquals(false, checkTypeOfDataController.isDouble("123TestString"));
+        assertTrue(CheckTypeOfDataController.isDouble("2.043"));
+        assertTrue(CheckTypeOfDataController.isDouble("2.044E-10"));
+        assertTrue(CheckTypeOfDataController.isDouble("-2.045"));
+        assertTrue(CheckTypeOfDataController.isDouble("-2.046E-10"));
+        assertTrue(CheckTypeOfDataController.isDouble("0.0"));
+        assertFalse(CheckTypeOfDataController.isDouble("2043"));
+        assertFalse(CheckTypeOfDataController.isDouble("123TestString"));
     }
 
     @Test
     void isIntegerTest(){
-        assertEquals(true, checkTypeOfDataController.isLong("2"));
-        assertEquals(true, checkTypeOfDataController.isLong("-2"));
-        assertEquals(true, checkTypeOfDataController.isLong("0"));
-        assertEquals(false, checkTypeOfDataController.isLong("1.00"));
-        assertEquals(false, checkTypeOfDataController.isLong("0.00"));
-        assertEquals(false, checkTypeOfDataController.isLong("-1.000E-10"));
-        assertEquals(false, checkTypeOfDataController.isLong("String"));
-        assertEquals(false, checkTypeOfDataController.isLong("String123"));
+        assertTrue(CheckTypeOfDataController.isLong("2"));
+        assertTrue(CheckTypeOfDataController.isLong("-2"));
+        assertTrue(CheckTypeOfDataController.isLong("0"));
+        assertFalse(CheckTypeOfDataController.isLong("1.00"));
+        assertFalse(CheckTypeOfDataController.isLong("0.00"));
+        assertFalse(CheckTypeOfDataController.isLong("-1.000E-10"));
+        assertFalse(CheckTypeOfDataController.isLong("String"));
+        assertFalse(CheckTypeOfDataController.isLong("String123"));
     }
 
     @Test
     void isStringTest(){
-        assertEquals(true, checkTypeOfDataController.isString("TestString"));
-        assertEquals(false, checkTypeOfDataController.isString("123"));
-        assertEquals(false, checkTypeOfDataController.isString("123.123"));
-        assertEquals(false, checkTypeOfDataController.isString("123.123123E-10"));
-        assertEquals(false, checkTypeOfDataController.isString("-123"));
-        assertEquals(false, checkTypeOfDataController.isString("-123.123"));
-        assertEquals(false, checkTypeOfDataController.isString("-123.123123E-10"));
+        assertTrue(CheckTypeOfDataController.isString("TestString"));
+        assertFalse(CheckTypeOfDataController.isString("123"));
+        assertFalse(CheckTypeOfDataController.isString("123.123"));
+        assertFalse(CheckTypeOfDataController.isString("123.123123E-10"));
+        assertFalse(CheckTypeOfDataController.isString("-123"));
+        assertFalse(CheckTypeOfDataController.isString("-123.123"));
+        assertFalse(CheckTypeOfDataController.isString("-123.123123E-10"));
     }
 
 
