20c9f90be31b405bd5d3ee5fecfd3d7f7cc9956f
Evgenii Surikov <surikov96@mail.ru>
2025-01-21 15:20:31 +0700

    Correct check -o, -p null param

--- src/main/java/ru/surikov/controllers/ConfigurationSetController.java
+++ src/main/java/ru/surikov/controllers/ConfigurationSetController.java
@@ -12,7 +12,7 @@ public class ConfigurationSetController {
         try {
             for (int i = 0; i < args.length; i++) {
                 if (args[i].endsWith(".txt")) {
-                    config.addFileName(args[i]);
+                    config.addFileName(Paths.get(args[i]).toAbsolutePath().toString());
                 }
                 if (args[i].startsWith("-")) {
                     if (args[i].contains("s") && !config.getF()) {
@@ -29,23 +29,32 @@ public class ConfigurationSetController {
                     }
                     if (args[i].contains("o")) {
                         config.setO(true);
-                        if (args[i + 1].contains("\\")) {;
+                        if (args[i + 1].matches("^(?!-).*$")) {;
                             config.setSavePath(Paths.get(args[i + 1]).toAbsolutePath().toString());
-                        } else {
-                            throw new SystemPathParamNotFound("System path param not found");
                         }
                     }
                     if (args[i].contains("p")) {
                         config.setP(true);
                         if (args[i + 1].matches("^(?!-).*$")) {
                             config.setPrefix(args[i + 1]);
-                        } else {
-                            throw new PrefixNotFoundException("Prefix not found!");
                         }
                     }
                 }
             }
-        } catch (PrefixNotFoundException | SystemPathParamNotFound e) {
+
+            if(config.getP() && config.getPrefix() == null) {
+                config.setPrefix("");
+                throw new PrefixNotFoundException("Prefix not found!");
+            }
+            if(config.getO() && config.getSavePath() == null) {
+                throw new SystemPathParamNotFound("Save path not found! Please enter a save path after param -o");
+            }
+
+        } catch (PrefixNotFoundException e) {
+            config.setPrefix("");
+            System.out.println(e.getMessage());
+        } catch (SystemPathParamNotFound e) {
+            config.setSavePath("");
             System.out.println(e.getMessage());
         }
     }
