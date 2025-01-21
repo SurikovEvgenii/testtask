897519c9994189725b126a4e35741fc1c24919bd
Evgenii Surikov <surikov96@mail.ru>
2025-01-21 15:00:35 +0700

    Correct code

--- src/main/java/ru/surikov/Main.java
+++ src/main/java/ru/surikov/Main.java
@@ -7,13 +7,12 @@ import ru.surikov.handlers.FileHandler;
 
 public class Main {
     public static void main(String[] args) {
-        String[] argss = {"-f", "-c", "-a", "-d", "-p", "_static-" , "-o", "str\\else", "in1.txt", "in2.txt"};
-        Configuration configuration = Configuration.getInstance();
-        ConfigurationSetController.setConfig(argss, configuration);
+        Configuration configuration = new Configuration();
+        ConfigurationSetController.setConfig(args, configuration);
         System.out.println(configuration.toString());
         FileHandler.readFile(configuration.getListOfFileName());
         FileHandler.writeFile(configuration.getSavePath(), configuration.getPrefix(), configuration.getA());
-        StatisticsController.setStatistics(configuration.getS(), configuration.getA());
+        StatisticsController.setStatistics(configuration.getS(), configuration.getF());
         StatisticsController.viewStatistics(configuration.getS(), configuration.getF());
     }
 }
